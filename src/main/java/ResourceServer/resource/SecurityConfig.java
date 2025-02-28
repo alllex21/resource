7bebc57ac00afc7bf270a3ad931a10abb7db3e34
miladinovica <aleksandar.miladinovic@muehlbauer.de>
2025-02-28 12:57:45 +0100

    Overwrite JwtAuthenticationConverter bean to remove prefix and extract user authorities from rights

--- src/main/java/ResourceServer/resource/SecurityConfig.java
+++ src/main/java/ResourceServer/resource/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +24,17 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        // Set the claim name to extract authorities from (default is "scope")
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("rights");
+        // Optional: Remove the prefix (default is "SCOPE_")
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
 
 }
