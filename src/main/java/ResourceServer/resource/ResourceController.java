e9d9077cf2b45b2c2bb68b66b5b31c7edb0aa0df
milosevicis <Isidora.Milosevic@muehlbauer.de>
2025-02-28 13:18:37 +0100

    Add PreAuthorize to protectedEndpoint (EnableGlobalMethodSecurity annotation is used)

--- src/main/java/ResourceServer/resource/ResourceController.java
+++ src/main/java/ResourceServer/resource/ResourceController.java
@@ -1,6 +1,7 @@
 package ResourceServer.resource;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ResourceController {
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/protected")
-
     public ResponseEntity<String> protectedEndpoint() {
         return ResponseEntity.ok("You have access to this protected resource.");
     }
--- src/main/java/ResourceServer/resource/SecurityConfig.java
+++ src/main/java/ResourceServer/resource/SecurityConfig.java
@@ -3,6 +3,7 @@ package ResourceServer.resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
@@ -18,7 +20,6 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/protected").hasAnyAuthority("USER")
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
